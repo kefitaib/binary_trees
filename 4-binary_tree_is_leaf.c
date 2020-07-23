9925ffaf7f4a36c7527dbd47ba2cd7084ec31021
kefitaib <taibkefi@yahoo.fr>
2020-07-23 08:01:22 -0700

    checks if a node is a leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,9 +2,8 @@
 
 /**
  * binary_tree_is_leaf - checks if a node is a leaf
- * @parent : pointer to the parent node of the node to create
- * @value : value to put in the new node
- * Return: Pointer to the new node
+ * @node : pointer to the node to check
+ * Return: 1 if it's a leaf, otherwise 0
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
