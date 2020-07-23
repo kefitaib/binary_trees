c17b69633d9753956719b6e292adeeae90b241bc
kefitaib <taibkefi@yahoo.fr>
2020-07-23 13:55:22 -0700

    goes through a binary tree using post-order traversal

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using postorder traversal
  * @tree : binary tree
  * @func : Pointer to a function to print data
  * Return: Void
